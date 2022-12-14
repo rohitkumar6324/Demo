1645bb5de94ab5b25e3d1a3eeabe4b00020853ee
Rohit Kumar <kumarrohit6324@gmail.com>
2022-12-14 12:42:26 +0530

    pushed

---
+++ src/main/java/com/example/demo/DemoforstudentsApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoforstudentsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoforstudentsApplication.class, args);
+	}
+
+}
